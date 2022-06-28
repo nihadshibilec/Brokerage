f80c99dd9e418ca033053cd68fe84fd2794d39ad
Mohammed Nihad Shibil <108345955+nihadshibilec@users.noreply.github.com>
2022-06-28 22:53:24 +0530

    Add files via upload

---
+++ zerodha_brokerage.py
@@ -0,0 +1,30 @@
+def charges(buy,sell,qty):
+    turnover = (buy*qty)+(sell*qty)
+    t=turnover*0.0003
+    if(t<20):
+        brokerage = t*2
+    else:
+        brokerage = 40
+
+
+
+    STT = sell*qty*0.00025
+    transaction_charges = turnover*.0000345
+    gst =(brokerage+transaction_charges)*0.18
+    sebi_charges = sell*qty*0.000001*2
+    stamp_duty = buy*qty*0.00003
+
+    total = brokerage + STT + transaction_charges + gst + sebi_charges+stamp_duty
+
+
+
+    return(total,brokerage,STT,transaction_charges,gst,sebi_charges,stamp_duty)
+    
+
+
+print(charges(2200,2207,1000))
+    
+
+
+    
+
